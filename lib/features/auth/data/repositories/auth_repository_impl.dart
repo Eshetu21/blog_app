f1b094b2e5cc38fe434280cac4b3c3fe8fae0df0
Eshetu21 <eshetutesema2050@gmail.com>
2024-10-31 00:49:07 +0300

    refactoring

---
+++ lib/features/auth/data/repositories/auth_repository_impl.dart
@@ -0,0 +1,30 @@
+import 'package:blog_app/core/error/exceptions.dart';
+import 'package:blog_app/core/error/failures.dart';
+import 'package:blog_app/features/auth/data/datasources/auth_remote_data_source.dart';
+import 'package:blog_app/features/auth/domain/repository/auth_repository.dart';
+import 'package:fpdart/fpdart.dart';
+
+class AuthRepositoryImpl implements AuthRepository {
+  final AuthRemoteDataSource remoteDataSource;
+  const AuthRepositoryImpl(this.remoteDataSource);
+  @override
+  Future<Either<Failures, String>> loginWithEmailPassword(
+      {required String email, required String password}) {
+    // TODO: implement loginWithEmailPassword
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<Failures, String>> signupWithEmailPassword(
+      {required String name,
+      required String email,
+      required String password}) async {
+    try {
+      final userId = await remoteDataSource.signupWithEmailPassword(
+          name: name, email: email, password: password);
+      return right(userId);
+    } on ServerException catch (e) {
+      return left(Failures(e.message));
+    }
+  }
+}
